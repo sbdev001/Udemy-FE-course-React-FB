5b35229054badba76bedf1b647e2b39d2b0bd964
sbdev001 <sarvesh.bhardwaj@gmail.com>
2023-01-30 20:15:14 -0800

    JSX updating states

--- FirstReactSite/Tuts/my-first-react-app/src/App.js
+++ FirstReactSite/Tuts/my-first-react-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [name, setName] = useState('mario')
+
+  const handleClick = () => {
+    if (name === 'mario') {
+      setName('luigi')
+    } else {
+      setName('mario')
+    }
+    console.log(name)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React & Firebase part 2
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change name</button>
+      
     </div>
   );
 }
