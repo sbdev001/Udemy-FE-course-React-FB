3cfcfeee9ac67ead6d5b88dc17706cdddbfc0be7
sbdev001 <sarvesh.bhardwaj@gmail.com>
2023-02-05 12:01:54 -0800

    Using states

--- FirstReactSite/Tuts/my-first-react-app/src/App.js
+++ FirstReactSite/Tuts/my-first-react-app/src/App.js
@@ -2,22 +2,44 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [name, setName] = useState('mario')
 
-  const handleClick = () => {
-    if (name === 'mario') {
-      setName('luigi')
-    } else {
-      setName('mario')
-    }
-    console.log(name)
+  const [showEvents, setShowEvents] = useState(true)
+
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
+
+  console.log(showEvents)
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return (event.id !== id)
+    })
+  })
+    console.log(id)
   }
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
-      
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>Delete Event</button>
+        </div>
+      ))}
     </div>
   );
 }
