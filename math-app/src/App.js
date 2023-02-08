bd490bd75526f4ae66766082b521e82cc69a4988
sbdev001 <sarvesh.bhardwaj@gmail.com>
2023-02-08 08:05:52 -0800

    Working Math modules

--- math-app/src/App.js
+++ math-app/src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import { Mafs, Coordinates } from "mafs"
+import { MathJaxContext, MathJax } from 'better-react-mathjax';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MathJaxContext>
+              <h2>Basic MathJax example with Latex</h2>
+              <MathJax>{"\\(\\frac{10}{4x} \\approx 2^{12}\\)"}</MathJax>
+        </MathJaxContext>
       <Mafs>
       <Coordinates.Cartesian />
     </Mafs>
